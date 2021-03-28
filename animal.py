0767708a40aef85cd14925de7257af3e0ccbc049
xiaoqiw <81501400+xiaoqiw@users.noreply.github.com>
2021-03-28 10:33:23 -0400

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Xiaoqi Wu
+# Date: 3/28/2021
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
